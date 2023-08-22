da75211feb6b8706863036b28012f1ceb085bafd
NuminousBlue <jdiehl2236@gmail.com>
2023-08-21 23:03:18 -0500

    standard animation fully functional

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Iceburger from './lib/Iceburger'
 function App() {
   return (
     <>
-      <Iceburger size={3} color="rgba(0, 69, 169)" kind="standard" duration={300} lineThickness='standard' />
+      <Iceburger size={3} color="rgba(0, 69, 169)" kind="standard" duration={200} lineThickness='standard' />
     </>
   )
 }
